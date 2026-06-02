eb51ef09f8658997653c0c1301a3c171ac653761
hoducnamanh <hoducnamanh@gmail.com>
2026-06-02 15:41:39 +0700

    Add ViewModels

---
+++ KeToanGiaThanhSanPham/Models/ViewModels/SanPhamCreateViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace KeToanGiaThanhSanPham.Models.ViewModels
+{
+    public class SanPhamCreateViewModel
+    {
+    }
+}
---
+++ KeToanGiaThanhSanPham/Models/ViewModels/SanPhamEditViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KeToanGiaThanhSanPham.Models.ViewModels
+{
+    public class SanPhamEditViewModel
+    {
+    }
+}
